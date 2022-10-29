39c6eadd04fc78dae5fffa9cf4b366c5ba6d18fa
Shreya Sisodiya <shreyasisodiya@Shreyas-MacBook-Air.local>
2022-10-28 21:18:02 -0400

    save community

--- MEDICAL_RESOURCE_MANAGEMENT/src/views/CreateCommunity.java
+++ MEDICAL_RESOURCE_MANAGEMENT/src/views/CreateCommunity.java
@@ -4,6 +4,9 @@
  */
 package views;
 
+import static medical_resource_management.MEDICAL_RESOURCE_MANAGEMENT.communityMap;
+import models.Community;
+
 /**
  *
  * @author shreyasisodiya
@@ -32,9 +35,9 @@ public class CreateCommunity extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         comboCity = new javax.swing.JComboBox<>();
-        jComboBox3 = new javax.swing.JComboBox<>();
-        jButton11 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
+        buttonCommunitySave = new javax.swing.JButton();
+        textCommunityName = new javax.swing.JTextField();
+        textPin = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -53,12 +56,10 @@ public class CreateCommunity extends javax.swing.JFrame {
             }
         });
 
-        jComboBox3.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-
-        jButton11.setText("SAVE");
-        jButton11.addActionListener(new java.awt.event.ActionListener() {
+        buttonCommunitySave.setText("SAVE");
+        buttonCommunitySave.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton11ActionPerformed(evt);
+                buttonCommunitySaveActionPerformed(evt);
             }
         });
 
@@ -84,12 +85,12 @@ public class CreateCommunity extends javax.swing.JFrame {
                         .addGap(77, 77, 77)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(comboCity, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jComboBox3, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jTextField1)))
+                            .addComponent(textCommunityName)
+                            .addComponent(textPin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(288, 288, 288)
-                        .addComponent(jButton11)))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addComponent(buttonCommunitySave)))
+                .addContainerGap(308, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -103,15 +104,15 @@ public class CreateCommunity extends javax.swing.JFrame {
                 .addGap(41, 41, 41)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(textCommunityName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel3)
-                .addGap(46, 46, 46)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGap(43, 43, 43)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel5)
-                    .addComponent(jComboBox3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 47, Short.MAX_VALUE)
-                .addComponent(jButton11)
+                    .addComponent(textPin, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 50, Short.MAX_VALUE)
+                .addComponent(buttonCommunitySave)
                 .addGap(39, 39, 39))
         );
 
@@ -122,9 +123,14 @@ public class CreateCommunity extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_comboCityActionPerformed
 
-    private void jButton11ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton11ActionPerformed
+    private void buttonCommunitySaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonCommunitySaveActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton11ActionPerformed
+        Community c = new Community(textCommunityName.getText(), Integer.parseInt(textPin.getText()),comboCity.getSelectedItem().toString());
+        communityMap.put(c.getCommunityName(), c);
+        this.hide();
+        ViewSystemAdmin vsa = new ViewSystemAdmin();
+        vsa.show();
+    }//GEN-LAST:event_buttonCommunitySaveActionPerformed
 
     /**
      * @param args the command line arguments
@@ -162,14 +168,14 @@ public class CreateCommunity extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton buttonCommunitySave;
     public javax.swing.JComboBox<String> comboCity;
-    private javax.swing.JButton jButton11;
-    private javax.swing.JComboBox<String> jComboBox3;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel9;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField textCommunityName;
+    private javax.swing.JTextField textPin;
     // End of variables declaration//GEN-END:variables
 }
